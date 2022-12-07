46f4cd0f70017e564970b812c7b894470ddfd5f5
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-12-07 23:10:46 +0000

    added some comments that i forgot to put before

--- rgbNestedLoop.cpp
+++ rgbNestedLoop.cpp
@@ -27,7 +27,7 @@ int main() {
     std::cout << "Look at all these colours :) \n";
 
     // first while... loop to increment the first RGB value,
-    // R, or Red.
+    // R, or red.
     while (counter1 <= 255) {
         if (counter2 == 255 && counter3 == 255) {
             printf("\033[1;38;2;%i;%i;%i %i, %i, %i ",
@@ -36,6 +36,8 @@ int main() {
             std::cout << "\n";
             counter1 = counter1 + 1;
         }
+        // second while... loop to increment the second RGB value,
+        // G, or green.
         while (counter2 < 255) {
             if (counter3 == 255) {
                 printf("\033[1;38;2;%i;%i;%im %i, %i, %i ",
@@ -44,6 +46,8 @@ int main() {
                 std::cout << "\n";
                 counter2 = counter2 + 1;
             }
+            // third while... loop to increment the third RGB value,
+            // B, or blue.
             while (counter3 < 255) {
                 printf("\033[1;38;2;%i;%i;%im %i, %i, %i ",
                        counter1, counter2, counter3,
